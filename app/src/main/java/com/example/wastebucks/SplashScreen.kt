9b3ed4b62248ba617ae2e5ee571db91099dd01c8
blackhat_coder <ankit.21.kumar02@gmail.com>
2023-12-15 14:36:37 +0530

    initial commit

---
+++ app/src/main/java/com/example/wastebucks/SplashScreen.kt
@@ -0,0 +1,11 @@
+package com.example.wastebucks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
