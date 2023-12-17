098c342cc2fcea366432b7e8800e771387a93c0a
Apoorv Mittal <apoorvmitthu@gmail.com>
2023-12-18 00:06:44 +0530

    Bottom Nav Bar

--- app/src/main/java/com/example/wastebucks/MainActivity.kt
+++ app/src/main/java/com/example/wastebucks/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.wastebucks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.wastebucks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(Home())
+                R.id.community -> replaceFragment(Community())
+                R.id.pickup -> replaceFragment(Pickup())
+                R.id.profile -> replaceFragment(Profile())
+
+                else -> {}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
