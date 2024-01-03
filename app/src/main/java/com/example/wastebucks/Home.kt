dc35fd3befbc28d1435bf9b139b70e218ec7b466
Ankit Kumar <120427738+blackhat-coder21@users.noreply.github.com>
2024-01-03 15:16:48 +0530

    Revert "Added google sign in"

--- app/src/main/java/com/example/wastebucks/Home.kt
+++ app/src/main/java/com/example/wastebucks/Home.kt
@@ -1,17 +1,10 @@
 package com.example.wastebucks
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,8 +20,6 @@ class Home : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var auth:FirebaseAuth
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,17 +29,6 @@ class Home : Fragment() {
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-            val signOutButton = view.findViewById<Button>(R.id.signOutButton)
-            auth = Firebase.auth
-            Log.d("User Details", "${auth.currentUser}")
-            if (auth.currentUser != null) {
-                startActivity(Intent(view.context, LoginActivity::class.java))
-            }
-
-    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -56,7 +36,6 @@ class Home : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
-
 
     companion object {
         /**
@@ -78,4 +57,3 @@ class Home : Fragment() {
             }
     }
 }
-
