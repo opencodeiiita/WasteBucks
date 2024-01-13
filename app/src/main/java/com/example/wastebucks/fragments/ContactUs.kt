f1d0ee3f03680dc75f9a1cfcd3d4d3dd13456b74
Monu Kumar Singh <monusinghbharadwaj@gmail.com>
2024-01-13 12:10:41 +0530

    ContactUs

--- app/src/main/java/com/example/wastebucks/fragments/ContactUs.kt
+++ app/src/main/java/com/example/wastebucks/fragments/ContactUs.kt
@@ -1,25 +1,18 @@
-package com.example.wastebucks
+package com.example.wastebucks.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import com.example.wastebucks.fragments.ContactUs
+import androidx.fragment.app.Fragment
+import com.example.wastebucks.R
 
-class Profile : Fragment() {
-
-
+class ContactUs : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.fragment_contact_us, container, false)
     }
-
-
 }
-
-
